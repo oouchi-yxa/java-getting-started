14b633449459ec38feff2200e8c8e8e60661b00d
oouchi-yxa <oouchi-yxa@nec.com>
2024-01-14 19:51:56 +0900

    RestTemplateの雛形追加

---
+++ src/main/java/com/heroku/java/cms/CmsFileStatus.java
@@ -0,0 +1,121 @@
+package com.heroku.java.cms;
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Controller
+@Log
+public class CmsFileStatus {
+
+    @GetMapping("/cms/input")
+    public String mailInput()
+    {
+        return "cms/input";
+    }
+
+    @GetMapping(value = "/cmsFileStatus/{filePath}")
+    public String cmsFileStatus(
+            @PathVariable String filePath,
+            Model model) {
+
+        CmsSetting cmsSetting = new CmsSetting();
+
+        // CloudCube設定の参照
+        String cloudcubeAccessKeyId = cmsSetting.getAccess_key_id();
+        String cloudcubeSecretAccessKey = cmsSetting.getSecret_access_key();
+        String cloudcubeUrl = cmsSetting.getUrl();
+
+        System.setProperty("aws.accessKeyId", cloudcubeAccessKeyId);
+        System.setProperty("aws.secretAccessKey", cloudcubeSecretAccessKey);
+
+        model.addAttribute("message", "");
+
+        // 試しに組み込み
+        S3Client s3Client =
+                S3Client.builder()
+                        .credentialsProvider(DefaultCredentialsProvider.create())
+                        .region(Region.US_EAST_1)
+                        .build();
+
+        try {
+            // 設定値取り出し
+            Pattern p = Pattern.compile("^https://(.*)\\.s3\\.amazonaws\\.com/(.*)$");
+            Matcher m = p.matcher(cloudcubeUrl);
+            String bucket = "";
+            String basePrefix = "";
+            if (m.find()){
+                bucket = m.group(1);
+                basePrefix = m.group(2);
+            }
+
+            // リスト参照
+            ListObjectsRequest listObjects = ListObjectsRequest
+                    .builder()
+                    .bucket(bucket)
+                    .prefix(basePrefix + filePath)
+                    .build();
+
+            ListObjectsResponse res = s3Client.listObjects(listObjects);
+            List<S3Object> objects = res.contents();
+            if (objects.size() == 0) {
+                model.addAttribute("message", filePath + "is empty.");
+            }
+            String tmp = "";
+            for (S3Object myValue : objects) {
+                log.info("\n The name of the key is " + myValue.key());
+                log.info("\n The owner is " + myValue.owner());
+
+                tmp +=  myValue.key() + ":" + myValue.owner() + "<br>\n";
+            }
+            model.addAttribute("message", tmp);
+
+            /*
+            System.out.print("\n data get 0");
+
+            GetObjectRequest objectRequest = GetObjectRequest
+                    .builder()
+                    .key("hxlzsmew3hjg/public/tree_sample.html")
+                    .bucket("cloud-cube-us2")
+                    .build();
+
+            System.out.print("\n data get 1");
+
+            ResponseBytes<GetObjectResponse> objectBytes = s3Client.getObjectAsBytes(objectRequest);
+            byte[] data = objectBytes.asByteArray();
+
+            System.out.print("\n data get 2");
+
+            // Write the data to a local file.
+            File myFile = new File("/tmp/test.txt");
+            OutputStream os = new FileOutputStream(myFile);
+            os.write(data);
+            System.out.println("Successfully obtained bytes from an S3 object");
+            os.close();
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+             */
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+
+        return "cms/fileStatus";
+    }
+
+}
---
+++ src/main/java/com/heroku/java/cms/CmsSetting.java
@@ -0,0 +1,16 @@
+package com.heroku.java.cms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CmsSetting {
+
+    String access_key_id = System.getenv("CLOUDCUBE_ACCESS_KEY_ID");
+
+    String secret_access_key = System.getenv("CLOUDCUBE_SECRET_ACCESS_KEY");
+
+    String url = System.getenv("CLOUDCUBE_URL");
+
+}
